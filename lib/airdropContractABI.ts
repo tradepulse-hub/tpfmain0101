1f617e5307287f9944cb3b40e832c1f141549f8a
tradepulse-hub <support@tradepulsetoken.com>
2025-06-30 20:33:23 +0100

    Update airdropContractABI.ts

--- lib/airdropContractABI.ts
+++ lib/airdropContractABI.ts
@@ -1,7 +1,7 @@
 import { ethers } from "ethers"
 
-// Endereço do contrato de airdrop na Worldchain
-export const AIRDROP_CONTRACT_ADDRESS = "0x7b7540d8a1713a5c7d7C9257573Bdf56E7488E05"
+// Novo endereço do contrato de airdrop na Worldchain
+export const AIRDROP_CONTRACT_ADDRESS = "0x0089b777aa68589E115357313BDbBa2F662c81Bf"
 
 // Endereço do token TPF
 export const TPF_TOKEN_ADDRESS = "0x834a73c0a83F3BCe349A116FFB2A4c2d1C651E45"
@@ -14,7 +14,7 @@ export const RPC_ENDPOINTS = [
   "https://rpc-testnet.worldcoin.org",
 ]
 
-// ABI completa do contrato de airdrop
+// ABI completa do contrato de airdrop atualizada
 export const airdropContractABI = [
   {
     inputs: [],
@@ -41,12 +41,99 @@ export const airdropContractABI = [
     type: "event",
   },
   {
-    inputs: [],
-    name: "CLAIM_INTERVAL",
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "user",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "bool",
+        name: "blocked",
+        type: "bool",
+      },
+    ],
+    name: "AddressBlocked",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "oldAmount",
+        type: "uint256",
+      },
+      {
+        indexed: false,
+        internalType: "uint256",
+        name: "newAmount",
+        type: "uint256",
+      },
+    ],
+    name: "DailyAirdropUpdated",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "blockedAddresses",
     outputs: [
       {
-        internalType: "uint256",
+        internalType: "bool",
         name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "user",
+        type: "address",
+      },
+    ],
+    name: "canUserClaim",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "canClaim",
+        type: "bool",
+      },
+      {
+        internalType: "uint256",
+        name: "timeUntilNextClaim",
         type: "uint256",
       },
     ],
@@ -55,7 +142,7 @@ export const airdropContractABI = [
   },
   {
     inputs: [],
-    name: "DAILY_AIRDROP",
+    name: "CLAIM_INTERVAL",
     outputs: [
       {
         internalType: "uint256",
@@ -75,12 +162,92 @@ export const airdropContractABI = [
   },
   {
     inputs: [],
+    name: "claimAirdropSafe",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
     name: "contractBalance",
     outputs: [
       {
         internalType: "uint256",
         name: "",
         type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "dailyAirdrop",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "emergencyPaused",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "getContractInfo",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "currentDailyAirdrop",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "currentBalance",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "claimInterval",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "tokenAddress",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "user",
+        type: "address",
+      },
+    ],
+    name: "isAddressBlocked",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
       },
     ],
     stateMutability: "view",
@@ -119,6 +286,68 @@ export const airdropContractABI = [
     type: "function",
   },
   {
+    inputs: [
+      {
+        internalType: "address",
+        name: "user",
+        type: "address",
+      },
+      {
+        internalType: "bool",
+        name: "blocked",
+        type: "bool",
+      },
+    ],
+    name: "setAddressBlocked",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "newAmount",
+        type: "uint256",
+      },
+    ],
+    name: "setDailyAirdrop",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bool",
+        name: "paused",
+        type: "bool",
+      },
+    ],
+    name: "setEmergencyPause",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address[]",
+        name: "users",
+        type: "address[]",
+      },
+      {
+        internalType: "bool",
+        name: "blocked",
+        type: "bool",
+      },
+    ],
+    name: "setMultipleAddressesBlocked",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
     inputs: [],
     name: "tpfTokenAddress",
     outputs: [
@@ -132,8 +361,34 @@ export const airdropContractABI = [
     type: "function",
   },
   {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
     inputs: [],
     name: "withdrawExcessTPF",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    name: "withdrawTPF",
     outputs: [],
     stateMutability: "nonpayable",
     type: "function",
